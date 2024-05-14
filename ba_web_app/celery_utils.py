2b6fe2bae4960799218e0263ddb41f04ab8ffcfe
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2024-05-14 16:48:36 -0700

    cleanup

--- ba_web_app/celery_utils.py
+++ ba_web_app/celery_utils.py
@@ -21,16 +21,3 @@ def init_celery(app):
             with app.app_context():
                 return TaskBase.__call__(self, *args, **kwargs)
     celery.Task = ContextTask
-
-
-# def celery_init_app(app: Flask) -> Celery:
-#     class FlaskTask(Task):
-#         def __call__(self, *args: object, **kwargs: object) -> object:
-#             with app.app_context():
-#                 return self.run(*args, **kwargs)
-#
-#     celery_app = Celery(app.name, task_cls=FlaskTask)
-#     celery_app.config_from_object(app.config["CELERY"])
-#     celery_app.set_default()
-#     app.extensions["celery"] = celery_app
-#     return celery_app
